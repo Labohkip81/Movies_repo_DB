f773d6fa477348be947e8b7e0ce5a7ac45f8bf9f
labohkip81 <labankiplagat81@gmail.com>
2019-07-24 12:49:25 +0300

    Created urlconf for the core application.

--- django/core/models.py
+++ django/core/models.py
@@ -7,19 +7,19 @@ class Movie(models.Model):
     RATED_G = 1
     RATED_PG = 2
     RATED_R = 3
-  #  RATINGS = (
-     #   (NOT_RATED, 'NR - Not Rated '),
-      #  (RATED_G,'G - General Audiences'),
-       # (RATED_PG, 'PG - Parental Guidance'),
-       # (RATED_R, 'R - Restricted'),
-  #  )
+    RATINGS = (
+        (NOT_RATED, 'NR - Not Rated '),
+        (RATED_G,'G - General Audiences'),
+        (RATED_PG, 'PG - Parental Guidance'),
+        (RATED_R, 'R - Restricted'),
+    )
     title = models.CharField(max_length=140)
     plot = models.TextField()
     year = models.PositiveIntegerField()
-   # rating = models.IntegerField(
-   #     choices=RATINGS,
-    #    default=NOT_RATED
-   # )
+    rating = models.IntegerField(
+        choices=RATINGS,
+        default=NOT_RATED
+    )
     runtime = models.PositiveIntegerField()
     website = models.URLField(
         blank=True)
