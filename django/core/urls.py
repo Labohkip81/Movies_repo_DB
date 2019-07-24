f442f9bb8a48f82432ce1b85bf3f9568fed80b79
labohkip81 <labankiplagat81@gmail.com>
2019-07-24 15:59:16 +0300

    Displayed the movie details as provided by the user in the movie detail page.

--- django/core/urls.py
+++ django/core/urls.py
@@ -6,4 +6,5 @@ app_name = 'core'
 
 urlpatterns = [
     path('movies', views.MovieList.as_view(), name= 'MovieList'),
+    path('movie/<int:pk>', views.MovieDetail.as_view(), name='MovieDetail'),
 ]
