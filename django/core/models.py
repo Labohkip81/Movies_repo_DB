ddae1536a16eca2c142ec98ec7223a15a95d70b8
labohkip81 <labankiplagat81@gmail.com>
2019-07-24 12:30:11 +0300

    Rectified the issue that arose from  django verions conflict.

--- django/core/models.py
+++ django/core/models.py
@@ -7,24 +7,22 @@ class Movie(models.Model):
     RATED_G = 1
     RATED_PG = 2
     RATED_R = 3
-    RATINGS = (
-        (NOT_RATED, 'NR - Not Rated '),
-        (RATED_G,'G - General Audiences'),
-        (RATED_PG, 'PG - Parental Guidance'),
-        (RATED_R, 'R - Restricted'),
-    )
+  #  RATINGS = (
+     #   (NOT_RATED, 'NR - Not Rated '),
+      #  (RATED_G,'G - General Audiences'),
+       # (RATED_PG, 'PG - Parental Guidance'),
+       # (RATED_R, 'R - Restricted'),
+  #  )
     title = models.CharField(max_length=140)
     plot = models.TextField()
     year = models.PositiveIntegerField()
-    rating = models.IntegerField(
-        choices=RATINGS,
-        default=NOT_RATED
-    )
-    runtime = \
-        models.PositiveIntegerField()
+   # rating = models.IntegerField(
+   #     choices=RATINGS,
+    #    default=NOT_RATED
+   # )
+    runtime = models.PositiveIntegerField()
     website = models.URLField(
         blank=True)
     def __str__(self):
         return '{} ({})'.format(
-            self.title, self.year
-        )
+            self.title,self.year)
